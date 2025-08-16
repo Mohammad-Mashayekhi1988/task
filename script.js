c9a6da381714df68b8bde7cebad86b43f2d07fd4
mashayekhi <mm.mashayekhi1988@gmail.com>
2025-08-16 10:42:04 +0330

    add slider

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// slider
+
+let img = document.querySelector('.img-slider')
+let nextElem = document.querySelector('.next')
+let prevElem = document.querySelector('.prev')
+let arraySlider = ["images/dentice/1.jpg",
+   "images/dentice/2.jpg", "images/dentice/3.jpg",
+    "images/dentice/4.jpg", "images/dentice/5.jpg"]
+let index = 0;
+function next() {
+    index--;
+    if (index < 0) {
+        index = arraySlider.length - 1;
+    }
+    img.setAttribute('src', arraySlider[index])
+}
+
+function prev() {
+    index++;
+    if (index > arraySlider.length - 1) {
+        index = 0;
+    }
+    img.setAttribute('src', arraySlider[index])
+}
+nextElem.addEventListener('click', next)
+prevElem.addEventListener('click', prev)
+setInterval(next, 3000)
+
+
 document.querySelectorAll(".dropBtn").forEach((btn) => {
   const content = btn.nextElementSibling;
 
@@ -8,8 +37,7 @@ document.querySelectorAll(".dropBtn").forEach((btn) => {
     });
 
     // close/open menu
-    content.style.display =
-      content.style.display === "block" ? "none" : "block";
+    content.style.display = content.style.display === "block" ? "none" : "block";
 
     e.stopPropagation();
   });
@@ -17,9 +45,7 @@ document.querySelectorAll(".dropBtn").forEach((btn) => {
 
 // close menu click out
 document.addEventListener("click", () => {
-  document
-    .querySelectorAll(".dropContent")
-    .forEach((c) => (c.style.display = "none"));
+  document.querySelectorAll(".dropContent").forEach((c) => (c.style.display = "none"));
 });
 
 const btn = document.getElementById("menubtn");
@@ -28,6 +54,7 @@ const menu = document.getElementById("dropdown");
 btn.addEventListener("click", () => {
   menu.classList.toggle("open");
 });
+
 
 document.querySelectorAll(".section-five p").forEach((p) => {
   p.addEventListener("click", () => {
